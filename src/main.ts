a32a0d045b5009ba000732ab5d89198668708933
Rafael Papastamatiou <rafael@teamhub.com.br>
2021-05-04 14:40:57 -0300

    swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('GoRestaurant')
+    .setDescription('GoRestaurant API')
+    .setVersion('1.0')
+    .addTag('restaurant')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, document);
+  SwaggerModule.setup('', app, document);
+
+  app.enableCors();
+
   await app.listen(3000);
 }
 bootstrap();
