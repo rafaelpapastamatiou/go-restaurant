1e97791ee621833384358369c37cae5b88e3b212
Rafael Papastamatiou <rafael@teamhub.com.br>
2021-05-04 13:42:07 -0300

    first commit

---
+++ src/modules/auth/entities/user.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({ type: 'boolean', default: 0 })
+  admin: boolean;
+}
